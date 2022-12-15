44fd75cb22e893a401d9780241289cefaebebb10
Tait Hoyem <tait@tait.tech>
2022-12-14 18:47:15 -0700

    Change introspection file to generate Rust code instead of C/C.h code

--- xml/generate-introspection.py
+++ xml/generate-introspection.py
@@ -1,104 +1,33 @@
-#!/usr/bin/env python3
-#
-# Takes DBus XML files and writes out a pair of introspection.[ch] files for inclusion
-# in C code.
+import xml.etree.ElementTree as ET
+tree = ET.parse("../xml/Event.xml")
+root = tree.getroot()
 
-import argparse
-import os
-import sys
-from xml.etree import ElementTree
+RUST_TYPE_FROM_DBUS_TYPE = {
+  "s": "String",
+  "i": "i32",
+  "u": "u32",
+  "v": "OwnedValue",
+  "a{sv}": "HashMap<String, OwnedValue>"
+}
 
-CTEMPLATE = """
-/*
- * This file has been auto-generated from the introspection data available
- * in the at-spi2-core repository. The D-Bus protocol is defined in this
- * repository, which can be found at:
- *
- * https://gitlab.gnome.org/GNOME/at-spi2-core
- *
- * DO NOT EDIT.
- */
+for interface in root:
+  enum_name = interface.attrib["name"].split(".")[-1] + "Event"
+  print(f"pub enum {enum_name}")
+  structs = []
+  for signal in interface:
+    variant_name = signal.attrib["name"]
+    print(f"\t{variant_name}({variant_name}Event),")
+    struct = f"struct {variant_name}Event {{\n"
+    for arg in signal:
+      if "name" in arg.attrib and arg.tag == "arg":
+        field_name = arg.attrib["name"]
+        field_type = arg.attrib["type"]
+        rust_type = RUST_TYPE_FROM_DBUS_TYPE[field_type]
+        struct += f"\t{field_name}: {rust_type},\n"
+    struct += "}"
+    structs.append(struct)
+  print("}")
+  for struct in structs:
+    print(struct)
 
-%s
-"""
 
-HTEMPLATE = """
-/*
- * This file has been auto-generated from the introspection data available
- * in the at-spi2-core repository. The D-Bus protocol is defined in this
- * repository, which can be found at:
- *
- * https://gitlab.gnome.org/GNOME/at-spi2-core
- *
- * DO NOT EDIT.
- */
-
-#ifndef SPI_INTROSPECTION_DATA_H_
-#define SPI_INTROSPECTION_DATA_H_
-
-%s
-
-#endif /* SPI_INTROSPECTION_DATA_H_ */
-"""
-
-DECTEMPLATE = """
-extern const char *%s;
-"""
-
-DEFTEMPLATE = """
-const char *%s =
-%s;
-"""
-
-def convert_name (name):
-    return "spi_" + name.replace (".", "_")
-
-def convert_contents (contents):
-    contents = contents.replace ("\"", "\\\"")
-    literals = ["\"%s\"" % (line) for line in contents.split ("\n")]
-    return "\n".join (literals)
-
-def generate_introspection (inputs, c_output_filename, h_output_filename):
-    #Open the output files.
-    cfile = open (c_output_filename, "w")
-    hfile = open (h_output_filename, "w")
-
-    ccontents = ""
-    hcontents = ""
-
-    for input_filename in inputs:
-        #Open the XML file and process includes.
-        try:
-            tree = ElementTree.parse (input_filename)
-        except Exception as e:
-            raise type(e)(f"Invalid XML while parsing {input_filename}: {str(e)}")
-
-        root = tree.getroot ()
-
-        for itf in root.findall ("interface"):
-            #Get and convert the name of the interface.
-            name = convert_name (itf.attrib["name"])
-
-            contents = convert_contents (ElementTree.tostring (itf, encoding="unicode"))
-
-            hcontents += DECTEMPLATE % (name)
-            ccontents += DEFTEMPLATE % (name, contents)
-
-    cfile.write (CTEMPLATE % (ccontents))
-    hfile.write (HTEMPLATE % (hcontents))
-
-    cfile.close ()
-    hfile.close ()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a C source file and header file from DBus XML files")
-    parser.add_argument('sources', metavar='FILE.XML', nargs='+', help='DBus XML interface file')
-    parser.add_argument('--c-output', metavar='OUT.C', required=True, help='Name out output C file')
-    parser.add_argument('--h-output', metavar='OUT.H', required=True, help='Name out output H file')
-    args = parser.parse_args()
-
-    input_filename = sys.argv[1]
-    c_output_filename = sys.argv[2]
-    h_output_filename = sys.argv[3]
-
-    generate_introspection (args.sources, args.c_output, args.h_output)
