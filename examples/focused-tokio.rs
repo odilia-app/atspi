ba4b83fc86ed54eccd541a270de219d7d2733be1
Luuk van der Duim <luukvanderduim@gmail.com>
2023-01-15 22:56:50 +0100

    [Examples] Adds `focused` examples
    This commit adds two examples using different executers, tokio and async-std.
    
    It also fixes a few glob-imports Clippy did notlike.

---
+++ examples/focused-tokio.rs
@@ -0,0 +1,35 @@
+use atspi::{
+    events::GenericEvent,
+    identify::object::StateChangedEvent,
+    signify::Signified,
+    zbus::{fdo::DBusProxy, MatchRule, MessageType},
+};
+use std::error::Error;
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let atspi = atspi::Connection::open().await?;
+    atspi.register_event("Object").await?;
+
+    let rule = MatchRule::builder()
+        .msg_type(MessageType::Signal)
+        .interface("org.a11y.atspi.Event.Object")?
+        .build();
+
+    let dbus = DBusProxy::new(atspi.connection()).await?;
+    dbus.add_match_rule(rule).await?;
+
+    let events = atspi.event_stream();
+    tokio::pin!(events);
+
+    while let Some(Ok(ev)) = events.next().await {
+        let Ok(change)  = <StateChangedEvent>::try_from(ev) else { continue; };
+
+        if change.kind() == "focused" && change.enabled() == 1 {
+            let Some(bus_name) = change.inner().sender()? else { continue; };
+            println!("Accessible belonging to {bus_name}  focused!");
+        }
+    }
+    Ok(())
+}
