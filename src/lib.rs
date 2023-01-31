a2ecb831d06b7ce3e40d83731962eeaceca68d63
Tait Hoyem <tait@tait.tech>
2023-01-31 01:25:17 -0700

    Add various *_ext modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,54 @@
 // #![deny(clippy::missing_docs)]
 
 pub mod accessible;
+pub mod accessible_ext;
 pub mod accessible_id;
 pub use accessible_id::*;
 
 pub mod action;
+pub mod action_ext;
 pub mod application;
+pub mod application_ext;
 pub mod bus;
 pub mod cache;
+pub mod cache_ext;
 pub mod collection;
+pub mod collection_ext;
 pub mod component;
+pub mod component_ext;
 pub mod convertable;
 pub mod device_event_controller;
+pub mod device_event_controller_ext;
 pub mod device_event_listener;
+pub mod device_event_listener_ext;
 pub mod document;
+pub mod document_ext;
 pub mod editable_text;
+pub mod editable_text_ext;
 pub mod events;
 pub mod identify;
 pub mod signify;
 pub use events::{Event, EventBody};
 pub mod hyperlink;
+pub mod hyperlink_ext;
 pub mod hypertext;
+pub mod hypertext_ext;
 pub mod image;
+pub mod image_ext;
 pub mod registry;
+pub mod registry_ext;
 pub mod selection;
+pub mod selection_ext;
 pub mod socket;
+pub mod socket_ext;
 pub mod table;
+pub mod table_ext;
 pub mod table_cell;
+pub mod table_cell_ext;
 pub mod text;
-pub mod value;
-
-pub mod accessible_ext;
 pub mod text_ext;
+pub mod value;
+pub mod value_ext;
 
 // Hand-written connection module
 mod connection;
