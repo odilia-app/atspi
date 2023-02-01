c6162e916b50cf7eec403b1de554efef56ae9b40
Tait Hoyem <tait@tait.tech>
2023-01-31 22:06:08 -0700

    TextExt compiles

--- src/text_ext.rs
+++ src/text_ext.rs
@@ -11,7 +11,7 @@ pub trait TextExtError: crate::text::Text {
 #[allow(clippy::module_name_repetitions)]
 pub trait TextBlockingExtError: crate::text::TextBlocking {
 	type Error: std::error::Error
-		+ From<<Self as crate::text::Text>::Error>;
+		+ From<<Self as crate::text::TextBlocking>::Error>;
 }
 
 #[async_trait]
@@ -31,7 +31,7 @@ impl<T: crate::text::Text + TextExtError + Send + Sync> TextExt for T {
     }
 }
 
-impl<T: crate::text::TextBlocking + TextBlockingExtError> TextExt for T {
+impl<T: crate::text::TextBlocking + TextBlockingExtError> TextBlockingExt for T {
     fn get_all_text(&self) -> Result<String, <T as TextBlockingExtError>::Error> {
         let length_of_string = self.character_count()?;
         Ok(self.get_text(0, length_of_string)?)
@@ -46,8 +46,13 @@ mod tests {
 	};
 
 	fn implements_text_ext<T: TextExt>() {}
+	fn implements_text_blocking_ext<T: TextBlockingExt>() {}
 	#[test]
 	fn test_text_proxy_implement_text_ext() {
 		implements_text_ext::<TextProxy<'static>>();
+	}
+	#[test]
+	fn test_blocking_text_proxy_implement_text_ext() {
+		implements_text_blocking_ext::<TextProxyBlocki9ng<'static>>();
 	}
 }
