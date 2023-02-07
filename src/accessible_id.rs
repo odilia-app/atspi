c2ce0aad4ec877c7a713f624b018a78772e5eb32
Tait Hoyem <tait@tait.tech>
2023-02-06 22:28:52 -0700

    Make AccessibleId serializable on the DBus wire

--- src/accessible_id.rs
+++ src/accessible_id.rs
@@ -1,17 +1,57 @@
 use zbus::zvariant::{
+  Error as ZvariantError,
+  Value,
+  Type,
 	OwnedValue,
 	ObjectPath,
 	OwnedObjectPath,
 	Signature,
 };
-use serde::{Serialize, Deserialize};
-use crate::error::ObjectPathConversionError;
+use serde::{Serialize, Deserialize, ser::Serializer, de::{Deserializer, Unexpected, self}};
+use crate::{
+  error::ObjectPathConversionError,
+};
 
-#[derive(Clone, Copy, Hash, Debug, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
+#[derive(Clone, Copy, Hash, Debug, Eq, PartialEq, PartialOrd, Ord, Type)]
+#[zvariant(signature = "o")]
 pub enum AccessibleId {
     Null,
     Root,
     Number(i64),
+}
+impl TryFrom<Value<'static>> for AccessibleId {
+  type Error = ZvariantError;
+  fn try_from(value: Value<'static>) -> Result<AccessibleId, Self::Error> {
+    match value {
+      Value::Str(path_id) =>
+        match path_id.as_str().try_into() {
+          Ok(id) => Ok(id),
+          Err(_) => Err(ZvariantError::Message("Incorrect string format; it must be in the format: /org/a11y/atspi/accessible/ID, where ID is between 0 and `i64::MAX`.".to_string())),
+        },
+      _ => Err(ZvariantError::Message("Invalid type to convert into an AccessibleId. It must be a Value::Str type".to_string())),
+    }
+  }
+}
+impl Serialize for AccessibleId {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> 
+    where S: Serializer {
+    serializer.serialize_str(&self.to_string())
+  }
+}
+impl<'de> Deserialize<'de> for AccessibleId {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> 
+  where
+    D: Deserializer<'de>,
+  {
+    let string_id = String::deserialize(deserializer)?;
+    match string_id.clone().try_into() {
+      Ok(id) => Ok(id),
+      Err(_e) => Err(de::Error::invalid_value(
+        Unexpected::Str(&string_id),
+        &"Format must be like /org/a11y/atspi/accessible/ID, where ID is some positive value from `i64::ZERO` to `i64::MAX`.",
+      ))
+    }
+  }
 }
 impl ToString for AccessibleId {
     fn to_string(&self) -> String {
@@ -22,6 +62,12 @@ impl ToString for AccessibleId {
         };
         format!("/org/a11y/atspi/accessible/{ending}")
     }
+}
+impl TryFrom<AccessibleId> for OwnedObjectPath {
+  type Error = zbus::zvariant::Error;
+  fn try_from(id: AccessibleId) -> Result<OwnedObjectPath, Self::Error> {
+    OwnedObjectPath::try_from(id.to_string())
+  }
 }
 impl<'a> TryInto<ObjectPath<'a>> for AccessibleId {
     type Error = zbus::zvariant::Error;
@@ -43,6 +89,21 @@ impl TryFrom<OwnedObjectPath> for AccessibleId {
                 Err(e) => Err(Self::Error::ParseError(e)),
             },
             None => Err(Self::Error::NoIdAvailable),
+        }
+    }
+}
+impl TryFrom<&str> for AccessibleId {
+	type Error = zbus::zvariant::Error;
+
+    fn try_from(path: &str) -> Result<Self, Self::Error> {
+        match path.split('/').next_back() {
+            Some("null") => Ok(AccessibleId::Null),
+            Some("root") => Ok(AccessibleId::Root),
+            Some(id) => match id.parse::<i64>() {
+                Ok(uid) => Ok(AccessibleId::Number(uid)),
+                Err(_) => Err(Self::Error::Message("Unable to parse the ID as part of a conversion from a String to an AccessibleId.".to_string())),
+            },
+            None => Err(Self::Error::Message("No ID in attempted conversion from a String to an AccessibleId".to_string())),
         }
     }
 }
