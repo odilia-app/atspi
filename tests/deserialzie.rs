c8c5fcc5cb1fa19b1facd4c48df1b91f39aa874c
Tait Hoyem <tait@tait.tech>
2023-02-08 00:58:35 -0700

    But deserialization tests for event names in the tests crate; no need to auto-generate tests

---
+++ tests/deserialzie.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+  use atspi::events::names::ObjectEvents;
+  use serde_plain;  
+
+  #[test]
+  fn serialize_object_variant() {
+    assert_eq!(serde_plain::to_string(&ObjectEvents::StateChanged).expect("Could not deserialize ObjectEvents::StateChanged"), "StateChanged".to_string());
+  }
+}
