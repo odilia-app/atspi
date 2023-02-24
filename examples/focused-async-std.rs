807aca68f00a580dca713cce5890fcf2aec4b03d
Tait Hoyem <tait@tait.tech>
2023-02-24 13:22:10 -0700

    Update examples

--- examples/focused-async-std.rs
+++ examples/focused-async-std.rs
@@ -8,7 +8,7 @@ use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	let atspi = atspi::AccessibilityBus::open().await?;
+	let atspi = atspi::AccessibilityConnection::open().await?;
 	atspi.register_event::<ObjectEvents>().await?;
 
 	let events = atspi.event_stream();
