605e14848d3f872ad90c1f9f2b656579d73254ae
Tait Hoyem <tait@tait.tech>
2023-06-11 17:22:12 +0800

    Move component types to atspi-common

--- atspi-proxies/src/component.rs
+++ atspi-proxies/src/component.rs
@@ -11,60 +11,7 @@
 //!
 
 use crate::atspi_proxy;
-use serde::{Deserialize, Serialize};
-use zbus::zvariant::Type;
-
-/// Indicates relative stacking order of a [`ComponentProxy`] with respect to the
-/// onscreen visual representation of the UI.
-///
-/// The layer index, in combination with the component's extents,
-/// can be used to compute the visibility of all or part of a component.
-/// This is important in programmatic determination of region-of-interest for magnification,
-/// and in flat screen review models of the screen, as well as for other uses.
-/// Objects residing in two of the `Layer` categories support further z-ordering information,
-/// with respect to their peers in the same layer:
-/// namely, [`Layer::Window`] and [`Layer::Mdi`].
-/// Relative stacking order for other objects within the same layer is not available;
-/// the recommended heuristic is first child paints first. In other words,
-/// assume that the first siblings in the child list are subject to being
-/// overpainted by later siblings if their bounds intersect.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize, Type)]
-pub enum Layer {
-	/// Indicates an error condition or uninitialized value.
-	Invalid,
-	/// Reserved for the desktop background; this is the bottom-most layer,
-	/// over which everything else is painted.
-	Background,
-	/// The 'background' layer for most content renderers and
-	/// UI [`ComponentProxy`] containers.
-	Canvas,
-	/// The layer in which the majority of ordinary 'foreground' widgets reside.
-	Widget,
-	/// A special layer between [`Layer::Canvas`] and [`Layer::Widget`], in which the
-	/// 'pseudo windows' (e.g. the Multiple-Document Interface frames) reside.
-	///
-	/// See [`ComponentProxy::get_mdizorder`].
-	Mdi,
-	/// A layer for popup window content, above [`Layer::Widget`].
-	Popup,
-	/// The topmost layer.
-	Overlay,
-	/// The layer in which a toplevel window background usually resides.
-	Window,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize, Type)]
-pub enum ScrollType {
-	TopLeft,
-	BottomRight,
-	TopEdge,
-	BottomEdge,
-	LeftEdge,
-	RightEdge,
-	Anywhere,
-}
-
-use crate::CoordType;
+use atspi_common::{CoordType, Layer, ScrollType};
 
 #[atspi_proxy(interface = "org.a11y.atspi.Component", assume_defaults = true)]
 trait Component {
