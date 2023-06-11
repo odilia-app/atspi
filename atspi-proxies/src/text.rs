b423c073968ca617705d6ec39cb34290ed1a7908
Tait Hoyem <tait@tait.tech>
2023-06-11 17:12:20 +0800

    Move text proxy types to common

--- atspi-proxies/src/text.rs
+++ atspi-proxies/src/text.rs
@@ -12,34 +12,8 @@
 #![allow(clippy::too_many_arguments)]
 // this is to silience clippy due to zbus expanding parameter expressions
 
-use crate::{atspi_proxy, CoordType};
-use serde::{Deserialize, Serialize};
-use zbus::zvariant::Type;
-
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Serialize, Deserialize, Type)]
-#[repr(u32)]
-/// Level of granularity to get text of, in relation to a cursor position.
-pub enum Granularity {
-	/// Gives the character at the index of the cursor. With a line-style cursor (which is standard) this will get the chracter that appears after the cursor.
-	Char,
-	/// Gives the entire word in front of or which contains the cursor. TODO: confirm that it always chooses the word in front of the cursor.
-	Word,
-	/// Gives to entire sentence in fron of the or which contains the cursor. TODO: confirm that it always chooses the sentence after the cursor.
-	Sentence,
-	/// Gives the line, as seen visually of which the cursor is situated within.
-	Line,
-	/// Gives the entire block of text, regardless of where the cursor lies within it.
-	Paragraph,
-}
-
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Serialize, Deserialize, Type)]
-#[repr(u32)]
-pub enum ClipType {
-	Neither,
-	Min,
-	Max,
-	Both,
-}
+use crate::atspi_proxy;
+use atspi_common::{Granularity, ClipType, CoordType};
 
 #[atspi_proxy(interface = "org.a11y.atspi.Text", assume_defaults = true)]
 trait Text {
