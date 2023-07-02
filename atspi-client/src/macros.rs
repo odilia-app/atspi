b215e37e1b4102a56d9b411e9f93f846c0ea0624
Tait Hoyem <tait@tait.tech>
2023-07-01 20:55:46 -0600

    Remove atspi-macros and atspi-client
    * atspi-macros was only required for the "interesting" functionality in atspi-client
    	* This was only enabled with the `unstable_traits` flag.
    	* It was a previous attmept to add universal functions to anything which shares the same trait as "Accessible".
    	* the `unstable_traits` flag would define a trait, "Accessible", and implement it for AccessibleProxy.
    	* Unfortunately, this ended up being a very clunky API.
    	* And was unused outside of Odilia.
    	* This functionality is better served by `_ext` crates, or future abstractions that are more obvious.
    * Time to remove this mess, and just make long-term maintainence simpler.

--- atspi-client/src/macros.rs
+++
@@ -1,7 +0,0 @@
-macro_rules! impl_extended_errors {
-	($proxy:ty, $error:ty) => {
-		impl $error for $proxy {
-			type Error = atspi_common::error::AtspiError;
-		}
-	};
-}
--- atspi-proxies/src/lib.rs
+++ atspi-proxies/src/lib.rs
@@ -7,10 +7,6 @@ compile_error!("You may not mix the async-std and tokio features.");
 #[cfg(all(not(feature = "async-std"), not(feature = "tokio")))]
 compile_error!("You must specify either the async-std or tokio feature.");
 
-#[cfg(feature = "unstable-traits")]
-use atspi_macros::atspi_proxy;
-
-#[cfg(not(feature = "unstable-traits"))]
 use zbus::dbus_proxy as atspi_proxy;
 
 pub mod accessible;
@@ -24,7 +20,7 @@ pub mod device_event_controller;
 pub mod device_event_listener;
 pub mod document;
 pub mod editable_text;
-pub use atspi_common::{events, CoordType, Interface, InterfaceSet};
+pub use atspi_common::{events, CoordType, Interface, InterfaceSet, AtspiError};
 
 pub mod hyperlink;
 pub mod hypertext;
@@ -36,12 +32,6 @@ pub mod table;
 pub mod table_cell;
 pub mod text;
 pub mod value;
-
-mod state;
-pub use state::*;
-
-pub mod error;
-pub use error::AtspiError;
 
 pub use zbus;
 
