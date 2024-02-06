b74f01a8ce6da592c72b9b7c0185a60648791ba7
Luuk van der Duim <luukvanderduim@gmail.com>
2024-02-06 16:21:08 +0100

    [Clippy] Warnings on multiple crate versions
    The following dependencies in the dependency tree are known to
    appear in multiple versions in the tree:
    
    async-lock (2.8.0, 3.3.0)
    socket2 (0.4.10, 0.5.5)
    futures-lite (1.13.0, 2.2.0)
    fastrand (1.9.0, 2.0.1)
    event-listener (4.0.3, 2.5.3)
    syn (1.0.109, 2.0.48)
    
    All these exist in different versions in dependencies of crates we depend on.

--- atspi-connection/src/lib.rs
+++ atspi-connection/src/lib.rs
@@ -2,6 +2,7 @@
 //!  connection may receive any [`atspi_common::events::Event`] structures.
 
 #![deny(clippy::all, clippy::pedantic, clippy::cargo, unsafe_code, rustdoc::all)]
+#![allow(clippy::multiple_crate_versions)]
 
 #[cfg(all(not(feature = "async-std"), not(feature = "tokio")))]
 compile_error!("You must specify at least one of the `async-std` or `tokio` features.");
