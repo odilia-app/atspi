7b99b9f92a1b61a1f22b768599c5ab0b1d3f7209
Luuk van der Duim <luukvanderduim@gmail.com>
2024-02-17 12:42:51 +0100

    [common] Bugfixes in Cache tests
    ```Rust
    	loop {
    		let to = events.try_next().await;
    		let event = to
    			.expect("stream timed out")
    			.expect("stream closed")
    			.expect("conversion to `Event` failed");
    
    		if let Event::Cache(CacheEvents::Remove(event)) = event {
    			let ObjectRef { name, path } = event.node_removed;
    			assert_eq!(name.as_str(), ":69.420");
    			assert_eq!(path.as_str(), "/org/a11y/atspi/accessible/remove");
    		}
    	}
    
    ```
    
    In the above pattern we could receive our synthetic signal, but the loop would
    carry on until the stream would timeout - or until an unrelated `Cache` event
    would be received.
    
    A simple `break;` fixes this.
    
    Secondly to prevent unrelated `Cache` events from triggering our event assertions,
    we now check whether the event sender name corresponds our connections' unique
    name.

--- atspi-common/tests/tests.rs
+++ atspi-common/tests/tests.rs
@@ -7,29 +7,28 @@ use tokio_stream::StreamExt;
 use zbus::Message;
 use zvariant::OwnedObjectPath;
 
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn test_recv_remove_accessible() {
 	let atspi = atspi_connection::AccessibilityConnection::new().await.unwrap();
 	atspi.register_event::<RemoveAccessibleEvent>().await.unwrap();
+	let unique_bus_name = atspi.connection().unique_name().unwrap();
 
 	let events = tokio_stream::StreamExt::timeout(atspi.event_stream(), Duration::from_secs(1));
 	tokio::pin!(events);
 
 	let msg = {
-		let unique_bus_name = atspi.connection().unique_name().unwrap();
-
 		let remove_body = ObjectRef {
 			name: ":69.420".into(),
 			path: OwnedObjectPath::try_from("/org/a11y/atspi/accessible/remove").unwrap(),
 		};
 
-		let path = "/org/a11y/atspi/accessible/null";
+		let path = "/org/a11y/atspi/accessible/cache";
 		let iface = "org.a11y.atspi.Cache";
 		let member = "RemoveAccessible";
 
 		zbus::Message::signal(path, iface, member)
 			.expect("Could not create signal")
-			.sender(unique_bus_name)
+			.sender(&unique_bus_name.clone())
 			.expect("Could not set sender")
 			.build(&remove_body)
 			.unwrap()
@@ -40,44 +39,52 @@ async fn test_recv_remove_accessible() {
 
 	loop {
 		let to = events.try_next().await;
-		let res = to.expect("Stream timed out").expect("Stream closed");
-		let event = res.expect("Error: conversion to Event failed");
+		let event = to
+			.expect("stream timed out")
+			.expect("stream closed")
+			.expect("conversion to `Event` failed");
 
 		if let Event::Cache(CacheEvents::Remove(event)) = event {
-			let removed_accessible = event.node_removed;
-			assert_eq!(removed_accessible.name.as_str(), ":69.420");
-			assert_eq!(removed_accessible.path.as_str(), "/org/a11y/atspi/accessible/remove");
-		} else {
-			continue;
+			// If we did not send the signal, continue listening.
+			if event.item.name.as_str() != unique_bus_name.as_str() {
+				continue;
+			}
+
+			let ObjectRef { name, path } = event.node_removed;
+			assert_eq!(name.as_str(), ":69.420");
+			assert_eq!(path.as_str(), "/org/a11y/atspi/accessible/remove");
+
+			// If we did, break the loop.
+			break;
 		}
 	}
 }
 
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn test_recv_add_accessible() {
 	let atspi = AccessibilityConnection::new().await.unwrap();
 	atspi.register_event::<AddAccessibleEvent>().await.unwrap();
+	let unique_bus_name = atspi.connection().unique_name().unwrap();
 
 	let events = tokio_stream::StreamExt::timeout(atspi.event_stream(), Duration::from_secs(1));
 	tokio::pin!(events);
 
 	let msg: zbus::Message = {
-		let path = "/org/a11y/atspi/accessible/null";
+		let path = "/org/a11y/atspi/cache";
 		let iface = "org.a11y.atspi.Cache";
 		let member = "AddAccessible";
 
-		let unique_bus_name = atspi.connection().unique_name().unwrap();
 		let add_body = CacheItem::default();
 
 		zbus::Message::signal(path, iface, member)
-			.expect("Could not create signal")
-			.sender(unique_bus_name.clone())
-			.expect("Could not set sender to {unique_bus_name:?}")
+			.expect("could not create signal")
+			.sender(&unique_bus_name.clone())
+			.expect("could not set sender")
 			.build(&add_body)
 			.unwrap()
 	};
 
-	assert_eq!(&msg.body().signature().unwrap(), &CACHE_ADD_SIGNATURE);
+	assert_eq!(msg.body().signature().unwrap(), CACHE_ADD_SIGNATURE);
 	atspi
 		.connection()
 		.send(&msg)
@@ -86,16 +93,22 @@ async fn test_recv_add_accessible() {
 
 	loop {
 		let to = events.try_next().await;
-		let opt = to.expect("Stream timed out");
-		let res = opt.expect("Stream closed");
-		let event = res.expect("Error: conversion to Event failed");
+		let event = to
+			.expect("stream timed out")
+			.expect("stream closed")
+			.expect("conversion to `Event` failed");
 
-		if let Event::Cache(CacheEvents::Add(AddAccessibleEvent { node_added, .. })) = event {
+		if let Event::Cache(CacheEvents::Add(AddAccessibleEvent { node_added, item })) = event {
+			// If we did not send the signal, continue listening.
+			if item.name.as_str() != unique_bus_name.as_str() {
+				continue;
+			}
 			assert_eq!(node_added.object.path.as_str(), "/org/a11y/atspi/accessible/object");
 			assert_eq!(node_added.app.path.as_str(), "/org/a11y/atspi/accessible/application");
 			assert_eq!(node_added.parent.path.as_str(), "/org/a11y/atspi/accessible/parent");
-		} else {
-			continue;
+
+			// If we did, break the loop.
+			break;
 		}
 	}
 }
@@ -107,6 +120,7 @@ async fn test_recv_add_accessible() {
 async fn test_recv_add_accessible_unmarshalled_body() {
 	let atspi = AccessibilityConnection::new().await.unwrap();
 	atspi.register_event::<AddAccessibleEvent>().await.unwrap();
+	let unique_bus_name = atspi.connection().unique_name().unwrap();
 
 	let events = tokio_stream::StreamExt::timeout(atspi.event_stream(), Duration::from_secs(1));
 	tokio::pin!(events);
@@ -116,24 +130,17 @@ async fn test_recv_add_accessible_unmarshalled_body() {
 		let iface = "org.a11y.atspi.Cache";
 		let member = "AddAccessible";
 
-		let unique_bus_name = atspi.connection().unique_name().unwrap();
-
 		let add_body = CacheItem::default();
 
 		Message::signal(path, iface, member)
-			.expect("Could not create signal")
-			.sender(unique_bus_name.clone())
-			.expect("Could not set sender to {unique_bus_name:?}")
-			.build(&(add_body,)) // Note the (unnecessary) outer parens
+			.expect("Could not create Builder for signal")
+			.sender(&unique_bus_name.clone())
+			.expect("Could not set sender")
+			.build(&(add_body)) // <--- Note the unmarshalled body
 			.unwrap()
 	};
 
-	assert_eq!(
-		&msg.body()
-			.signature()
-			.expect("Message body does not have `AddAccessible` signature"),
-		&CACHE_ADD_SIGNATURE
-	);
+	assert_eq!(&msg.body().signature().unwrap(), &CACHE_ADD_SIGNATURE);
 
 	atspi
 		.connection()
@@ -143,39 +150,23 @@ async fn test_recv_add_accessible_unmarshalled_body() {
 
 	loop {
 		let to = events.try_next().await;
-		assert!(to.is_ok(), "Stream timed out");
-		let opt = to.unwrap();
+		let event = to
+			.expect("stream timed out")
+			.expect("stream closed")
+			.expect("conversion to `Event` failed");
 
-		match opt {
-			Some(res) => {
-				// This result comes from inner event-stream, Stream yields a Result<Event, AtspiError>
-				match res {
-					Ok(event) => match event {
-						Event::Cache(CacheEvents::Add(AddAccessibleEvent {
-							item: _,
-							node_added: cache_item,
-						})) => {
-							assert_eq!(
-								cache_item.object.path.as_str(),
-								"/org/a11y/atspi/accessible/object"
-							);
-							assert_eq!(
-								cache_item.app.path.as_str(),
-								"/org/a11y/atspi/accessible/application"
-							);
-							assert_eq!(
-								cache_item.parent.path.as_str(),
-								"/org/a11y/atspi/accessible/parent"
-							);
-							break;
-						}
-						_any_other_event => continue,
-					},
-					Err(e) => panic!("Error: conversion to Event failed {e:?}"),
-				}
+		if let Event::Cache(CacheEvents::Add(AddAccessibleEvent { node_added, item })) = event {
+			// If we did not send the signal, continue listening.
+			if item.name.as_str() != unique_bus_name.as_str() {
+				continue;
 			}
-			// Stream yields a None when the stream is closed
-			None => panic!("Stream closed"),
+
+			assert_eq!(node_added.object.path.as_str(), "/org/a11y/atspi/accessible/object");
+			assert_eq!(node_added.app.path.as_str(), "/org/a11y/atspi/accessible/application");
+			assert_eq!(node_added.parent.path.as_str(), "/org/a11y/atspi/accessible/parent");
+
+			// If we did, break the loop.
+			break;
 		}
 	}
 }
