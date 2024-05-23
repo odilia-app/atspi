e8ffc00753c9011fea8843eb8495a2939761f02c
Tait Hoyem <tait@tait.tech>
2024-05-23 12:48:21 -0600

    Fix docs

--- atspi-common/src/macros.rs
+++ atspi-common/src/macros.rs
@@ -118,9 +118,9 @@ macro_rules! impl_from_user_facing_event_for_interface_event_enum {
 
 /// Expands to a conversion given two arguments,
 /// 1. the user facing event type `(inner_type)`
-/// which relies on a conversion to its interface variant enum type variant.
+///    which relies on a conversion to its interface variant enum type variant.
 /// 2. the outer `Event::<Interface(<InterfaceEnum>)>` wrapper.,
-/// the enum type and outtermost variant.
+///    the enum type and outtermost variant.
 ///
 /// ```ignore                                   user facing type, outer event variant
 /// impl_from_user_facing_type_for_event_enum!(StateChangedEvent, Event::Object);
