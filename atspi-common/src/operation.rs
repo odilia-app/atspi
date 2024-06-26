484e31ad09e1f23b72cac2899248fbae716ce7d3
Tait Hoyem <tait@tait.tech>
2024-06-25 21:44:11 -0600

    Add new Operation type for add/delete string types

---
+++ atspi-common/src/operation.rs
@@ -0,0 +1,33 @@
+/// An operation can either be [`Self::Insert`] or [`Self::Delete`].
+/// These correspond to methods available on [`Vec`].
+#[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize, Eq, Hash, Default)]
+pub enum Operation {
+	#[default]
+	#[serde(rename = "add")]
+	#[serde(alias = "add/system")]
+	Insert,
+	#[serde(rename = "delete")]
+	#[serde(alias = "delete/system")]
+	Delete,
+}
+
+impl TryFrom<&str> for Operation {
+	type Error = crate::AtspiError;
+	fn try_from(s: &str) -> Result<Operation, Self::Error> {
+		match s {
+			"add" | "add/system" => Ok(Operation::Insert),
+			"delete" | "delete/system" => Ok(Operation::Delete),
+			_ => Err(crate::AtspiError::KindMatch(format!("\"{s}\" is not a type of Operation"))),
+		}
+	}
+}
+
+impl Into<String> for Operation {
+	fn into(self) -> String {
+		match self {
+			Self::Insert => "add",
+			Self::Delete => "delete",
+		}
+		.to_string()
+	}
+}
