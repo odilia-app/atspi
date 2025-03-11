d721e79711fad3894d9351f63e6beea7599cec7a
Tait Hoyem <tait@tait.tech>
2025-03-11 08:22:57 -0600

    [WIP] idea for new benchmarks

--- atspi/benches/event_parsing_100k.rs
+++ atspi/benches/event_parsing_100k.rs
@@ -1,5 +1,6 @@
 use atspi::Event;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use std::time::Duration;
 
 mod bench_utils;
 use bench_utils::{generate_n_messages_rnd, read_messages_from_file, write_messages_to_file};
@@ -8,6 +9,7 @@ const FILE_PATH: &str = "100_000_messages.bin";
 const N: usize = 100_000;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+	let mut c = c.benchmark_group("msg_parse");
 	if !std::path::Path::new(FILE_PATH).exists() {
 		let random_messages = generate_n_messages_rnd(N);
 		write_messages_to_file(random_messages, FILE_PATH);
@@ -15,13 +17,25 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
 	let random_messages = read_messages_from_file(FILE_PATH);
 
+  c.sample_size(5_000);
+	c.measurement_time(Duration::from_secs(30));
+	c.throughput(Throughput::Elements(1));
 	c.bench_function("100_000 Messages into Events", |b| {
-		b.iter(|| {
-			for msg in &random_messages {
-				Event::try_from(black_box(msg)).unwrap();
-			}
-		})
+    let mut idx = 0;
+    let len = random_messages.len();
+		b.iter_batched(
+			|| {
+        let input = &random_messages[idx % len];
+        idx += 1;
+        input
+      },
+			|msg: &zbus::Message| {
+        black_box(Event::try_from(msg))
+			},
+			criterion::BatchSize::PerIteration
+		);
 	});
+	c.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
