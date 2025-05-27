a4158493c9f79605ae8fb686c60cc4773fa9a745
Tait Hoyem <tait@tait.tech>
2025-05-27 17:09:54 -0600

    Clarify async-io/tokio features

--- atspi-proxies/src/lib.rs
+++ atspi-proxies/src/lib.rs
@@ -4,8 +4,8 @@
 #![deny(clippy::all, clippy::pedantic, clippy::cargo, unsafe_code, rustdoc::all)]
 #![allow(clippy::multiple_crate_versions)]
 
-#[cfg(all(not(feature = "async-std"), not(feature = "tokio")))]
-compile_error!("You must specify at least one of the `async-std` or `tokio` features.");
+#[cfg(all(not(feature = "async-io"), not(feature = "tokio")))]
+compile_error!("You must specify at least one of the `async-io` or `tokio` features.");
 
 pub use atspi_common as common;
 
