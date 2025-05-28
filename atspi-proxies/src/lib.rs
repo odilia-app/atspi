6da43ac20deff5dbb3fd814353d2a9d4353f17d1
Tait Hoyem <tait@tait.tech>
2025-05-27 18:09:49 -0600

    Use async-io by default

--- atspi-proxies/src/lib.rs
+++ atspi-proxies/src/lib.rs
@@ -4,9 +4,6 @@
 #![deny(clippy::all, clippy::pedantic, clippy::cargo, unsafe_code, rustdoc::all)]
 #![allow(clippy::multiple_crate_versions)]
 
-#[cfg(all(not(feature = "async-io"), not(feature = "tokio")))]
-compile_error!("You must specify at least one of the `async-io` or `tokio` features.");
-
 pub use atspi_common as common;
 
 pub mod accessible;
