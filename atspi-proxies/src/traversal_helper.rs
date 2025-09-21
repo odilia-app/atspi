aaaa6a6140a5db2abfd8792949a318ac48ba8f60
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-09-21 15:01:14 -0400

    docs and example

--- atspi-proxies/src/traversal_helper.rs
+++ atspi-proxies/src/traversal_helper.rs
@@ -1,53 +1,70 @@
 use atspi_common::{AtspiError, State};
 use core::time;
 use std::collections::VecDeque;
+use std::time::Instant;
 
 use crate::accessible::{AccessibleProxy, ObjectRefExt};
 
-pub struct TraversalHelper {
-	pub root: AccessibleProxy<'static>,
+/// # [`TraversalHelper`]
+///
+/// A helper struct for clientside traversal of the accessibility tree.
+///
+/// Since most applications do not support the Collection interface,
+/// TraversalHelper allows for clean traversals without needing to
+/// implement tree algorithms yourself.
+pub struct TraversalHelper<'a> {
+	/// The root from which to start the traversal
+	pub root: AccessibleProxy<'a>,
+	// The connection to use for creating proxies
 	pub conn: zbus::Connection,
+	// The maximum depth to traverse to prevent excessively long traversals
 	pub max_depth: u32,
-	pub max_time: time::Duration,
+	// The maximum time to traverse to prevent excessively long traversals
+	pub max_time: Option<time::Duration>,
 }
 
-impl TraversalHelper {
+impl<'a> TraversalHelper<'a> {
 	#[must_use]
 	pub fn new(
-		root: AccessibleProxy<'static>,
+		root: AccessibleProxy<'a>,
 		conn: zbus::Connection,
 		max_depth: u32,
-		max_time: time::Duration,
-	) -> TraversalHelper {
+		max_time: Option<time::Duration>,
+	) -> TraversalHelper<'a> {
 		TraversalHelper { root, conn, max_depth, max_time }
 	}
 }
 
-// A set of helper methods that allow for simpler clientside traversal
-// of the accessibility tree. It is used sed since many applications
-// do not support the Collection proxy and thus we need to performan
-// the traversal ourselves
-pub trait TraversalExt {
-	/// The active descendant of the given object.
+/// A trait which mimics the org.a11y.atspi.Collection interface
+/// but entirely clientside. All methods perform dbus roundtrips.
+pub trait CollectionClientside {
+	/// Find the closest Accessible with State:Active starting from the root object
 	fn get_active_descendant(
 		&self,
 	) -> impl std::future::Future<Output = Result<AccessibleProxy<'_>, AtspiError>> + Send;
 }
 
-impl TraversalExt for TraversalHelper {
+impl<'a> CollectionClientside for TraversalHelper<'a> {
+	/// Find the closest Accessible with State:Active starting from the root object
 	async fn get_active_descendant(&self) -> Result<AccessibleProxy<'_>, AtspiError> {
 		let root = &self.root;
 		let mut queue = VecDeque::new();
 
+		let start = Instant::now();
+
 		queue.push_back((root.clone(), 0));
 
 		while let Some((node, current_depth)) = queue.pop_front() {
+			if let Some(max_time) = self.max_time {
+				if start.elapsed() > max_time {
+					return Err(AtspiError::TraversalTimeoutError);
+				}
+			}
 			if current_depth > self.max_depth {
 				continue;
 			}
 
-			let state = node.get_state().await?;
-			if state.contains(State::Active) {
+			if node.get_state().await?.contains(State::Active) {
 				return Ok(node);
 			}
 
---
+++ atspi/examples/clientside-collection-traversal.rs
@@ -0,0 +1,41 @@
+//! This example gets the root accessible object and
+//! traverses the accessibility tree to get the number
+//! of accessible objects for each running application
+//!
+//! ```sh
+//! cargo run --example clientside-collection-traversal
+//! ```
+//! Authors:
+//!    Colton Loftus
+use atspi::connection::set_session_accessibility;
+use atspi::proxy::accessible::ObjectRefExt;
+use atspi_proxies::traversal_helper::{TraversalExt, TraversalHelper};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> std::result::Result<(), Box<dyn Error>> {
+	set_session_accessibility(true).await?;
+	let atspi = atspi::AccessibilityConnection::new().await?;
+	let conn = atspi.connection();
+
+	let root = atspi.root_accessible_on_registry().await?;
+
+	// we can get the names of all applications currently running
+	for child in root.get_children().await?.iter() {
+		let traversal_helper = TraversalHelper::new(
+			child.to_owned().into_accessible_proxy(conn).await?,
+			conn.clone(),
+			200005,
+			None,
+		);
+		let result = traversal_helper.get_active_descendant().await;
+		if let Ok(proxy) = result {
+			println!(
+				"Got active descendant {} for app '{}'",
+				proxy.name().await?,
+				child.name().unwrap().as_str()
+			);
+		}
+	}
+	Ok(())
+}
