9dfabe94bd0c2f3389cf18eb95412279f96a38e3
Luuk van der Duim <luukvanderduim@gmail.com>
2026-06-05 14:12:35 +0200

    docs(proxies): Proper mod docs for `Component`

--- atspi-proxies/src/component.rs
+++ atspi-proxies/src/component.rs
@@ -1,23 +1,62 @@
 //! # `ComponentProxy`
 //!
-//! `org.a11y.atspi.Component` provides methods to obtain information about a
-//! UI component.
+//! A handle for a remote object implementing the `org.a11y.atspi.Component`
+//! interface.
+//!
+//! The `Component` interface provides methods to query and manipulate the visual
+//! and spatial aspects of a UI component. This includes retrieving its absolute
+//! and relative bounding box coordinates ([`get_extents`]), determining its
+//! layering order ([`get_layer`]), checking focus state, grabbing keyboard focus,
+//! and resolving which accessible element resides at a specific screen point ([`get_accessible_at_point`]).
 //!
 //! ## Defaults
 //!
-//! If "org.a11y.atspi.Component" is implemented, it is implemented per individual
-//! node in the application's UI-tree. This means the path will vary, so no default
-//! path is applicable for this proxy.
+//! The `Component` interface is implemented on individual, variable nodes within the
+//! application's UI-tree. As a consequence, the object path varies per node and
+//! no default path is applicable for this proxy.
 //!
-//! Path and service need to be provided by the builder or inherited from the
-//! [`zbus::Proxy`] this `ComponentProxy` is derived from.
+//! ## How to obtain a `ComponentProxy`
 //!
-//! Since no default path or service make sense for this proxy, the macro is
-//! instructed explicitly not to generate these fields.
+//! There are two idiomatic ways to obtain a `ComponentProxy`:
+//!
+//! ### 1. Safe conversion via [`ProxyExt`][pe] (Recommended)
+//! If you already have an [`AccessibleProxy`][ap] for a visual node, you can safely
+//! query and convert it using the [`ProxyExt`][pe] trait:
+//!
+//! ```rust,ignore
+//! use atspi::ProxyExt;
+//!
+//! let proxies = accessible_node.proxies().await?;
+//! let component = proxies.component().await?;
+//! ```
+//!
+//! All proxies obtained through [`ProxyExt`][pe] share their underlying
+//! [`zbus::Connection`], inheriting any P2P configuration if applicable.
+//!
+//! ### 2. Manual construction using the `builder`
+//! If you know the exact D-Bus service destination and object path, you can
+//! construct the proxy manually:
+//!
+//! ```rust,ignore
+//! let component = ComponentProxy::builder(&connection)
+//!     .destination(service_name)?
+//!     .path(object_path)?
+//!     .build()
+//!     .await?;
+//! ```
+//!
+//! [`get_extents`]: ComponentProxy#method.get_extents
+//! [`get_layer`]: ComponentProxy#method.get_layer
+//! [`get_accessible_at_point`]: ComponentProxy#method.get_accessible_at_point
+//! [pe]: crate::proxy_ext::ProxyExt
+//! [ap]: crate::accessible::AccessibleProxy
 
 use crate::common::{CoordType, Layer, ScrollType};
 use atspi_common::object_ref::ObjectRefOwned;
 
+// We don't want the proxy macro to auto-derive
+// defaults, so assume_defaults is explicitly
+// set to false.
 #[zbus::proxy(interface = "org.a11y.atspi.Component", assume_defaults = false)]
 pub trait Component {
 	/// `Contains` method
