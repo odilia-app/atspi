6c22c573fe86684d802facab0162f13b085d3013
Luuk van der Duim <luukvanderduim@gmail.com>
2026-06-07 22:09:44 +0200

    fix(proxies): `EditableText` compilable mod docs

--- atspi-proxies/src/editable_text.rs
+++ atspi-proxies/src/editable_text.rs
@@ -4,21 +4,40 @@
 //! interface. This interface provides access to and modification of the text
 //! content of editable text fields (such as text boxes or editors).
 //!
+//! ## Defaults
+//!
+//! The `EditableText` interface can be implemented on any individual node within the
+//! application's UI-tree (such as text input fields). As a consequence, the object path
+//! varies per node and no default path is applicable for this proxy.
+//!
 //! ## How to obtain an `EditableTextProxy`
 //!
-//! Because `EditableText` is implemented on individual, variable nodes within
-//! the UI-tree, you rarely instantiate it directly. Instead, there are three
-//! idiomatic ways to obtain a proxy:
+//! There are three idiomatic ways to obtain an `EditableTextProxy`:
 //!
 //! ### 1. Safe conversion via [`ProxyExt`][pe] (Recommended)
 //! If you already have an [`AccessibleProxy`][ap] for a node, you can safely query
 //! and convert it to an `EditableTextProxy` using the [`ProxyExt`][pe] trait:
 //!
-//! ```rust,ignore
-//! use atspi::ProxyExt;
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::proxy_ext::ProxyExt;
+//! use atspi_proxies::accessible::ObjectRefExt;
+//! use atspi_common::ObjectRefOwned;
 //!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! // Establish an `AccessibleProxy` for the node
+//! let obj_ref = ObjectRefOwned::from_static_str_unchecked(":1.1000", "/org/a11y/atspi/accessible/root");
+//! let accessible_node = obj_ref.into_accessible_proxy(&conn).await?;
+//!
+//! // Get the associated interface proxies
 //! let proxies = accessible_node.proxies().await?;
 //! let editable_text = proxies.editable_text().await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! Note that proxies obtained through [`ProxyExt`][pe] share their underlying
@@ -27,29 +46,60 @@
 //! will also share that same P2P `zbus::Connection`.
 //!
 //! ### 2. Fast resolution via [`AccessibilityConnection`][ac] (P2P)
-//! If the `p2p` feature is enabled and you have an [`ObjectRef`][or], you can resolve
+//! If the `p2p` feature is enabled and you have an [`ObjectRefOwned`][or], you can resolve
 //! it directly via the [`AccessibilityConnection`][ac] for maximum performance:
 //!
-//! ```rust,ignore
-//! let editable_text = connection.object_as_accessible(&object_ref).await?;
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::{AccessibilityConnection, P2P};
+//! use atspi_common::ObjectRefOwned;
+//! use atspi_proxies::proxy_ext::ProxyExt;
+//!
+//! # block_on( async {
+//! let connection = AccessibilityConnection::new().await?;
+//! let object_ref = ObjectRefOwned::from_static_str_unchecked(":1.1000", "/org/a11y/atspi/accessible/root");
+//!
+//! // Obtain the P2P connected `AccessibleProxy`
+//! let accessible = connection.object_as_accessible(&object_ref).await?;
+//!
+//! // Convert to `EditableTextProxy`
+//! let proxies = accessible.proxies().await?;
+//! let _editable_text = proxies.editable_text().await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! ### 3. Manual construction using the `builder`
 //! If you know the exact D-Bus service destination and object path, you can
 //! construct the proxy manually:
 //!
-//! ```rust,ignore
-//! let editable_text = EditableTextProxy::builder(&connection)
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::editable_text::EditableTextProxy;
+//! use zbus::proxy::CacheProperties;
+//!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! let bus_name = ":1.1001";
+//! let object_path = "/org/a11y/atspi/accessible/root";
+//!
+//! let editable_text = EditableTextProxy::builder(&conn)
 //!     .destination(bus_name)?
 //!     .path(object_path)?
+//!     .cache_properties(CacheProperties::No)
 //!     .build()
 //!     .await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
-//! [ac]: atspi-connection::AccessibilityConnection
-//! [ap]: atspi-proxies::AccessibilityProxy
+//! [ac]: https://docs.rs/atspi-connection/latest/atspi_connection/struct.AccessibilityConnection.html
+//! [ap]: crate::accessible::AccessibleProxy
 //! [pe]: crate::proxy_ext::ProxyExt
-//! [or]: atspi_common::ObjectRef
+//! [or]: atspi_common::object_ref::ObjectRefOwned
 //! [tp]: crate::text::TextProxy
 
 // The proxy macro attribute `assume_defaults = false` to avoid generating defaults service and path
