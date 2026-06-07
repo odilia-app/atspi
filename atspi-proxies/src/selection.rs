aa2fa8cb61cbca21227032788ad33ab80675333c
Luuk van der Duim <luukvanderduim@gmail.com>
2026-06-07 22:50:59 +0200

    fix(proxies): `Selection` compileable mod doc fragments

--- atspi-proxies/src/selection.rs
+++ atspi-proxies/src/selection.rs
@@ -12,7 +12,7 @@
 //!
 //! ## Defaults
 //!
-//! The `Selection` interface is implemented on individual, variable nodes within the
+//! The `Selection` interface can be implemented on any individual node within the
 //! application's UI-tree. As a consequence, the object path varies per node and
 //! no default path is applicable for this proxy.
 //!
@@ -24,11 +24,25 @@
 //! If you already have an [`AccessibleProxy`][ap] for a container node, you can safely
 //! query and convert it using the [`ProxyExt`][pe] trait:
 //!
-//! ```rust,ignore
-//! use atspi::ProxyExt;
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::proxy_ext::ProxyExt;
+//! use atspi_proxies::accessible::ObjectRefExt;
+//! use atspi_common::ObjectRefOwned;
+//!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! // Establish an `AccessibleProxy` for the container node
+//! let obj_ref = ObjectRefOwned::from_static_str_unchecked(":1.1000", "/org/a11y/atspi/accessible/root");
+//! let accessible_node = obj_ref.into_accessible_proxy(&conn).await?;
 //!
 //! let proxies = accessible_node.proxies().await?;
 //! let selection = proxies.selection().await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! All proxies obtained through [`ProxyExt`][pe] share their underlying
@@ -38,12 +52,27 @@
 //! If you know the exact D-Bus service destination and object path, you can
 //! construct the proxy manually:
 //!
-//! ```rust,ignore
-//! let selection = SelectionProxy::builder(&connection)
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::selection::SelectionProxy;
+//! use zbus::proxy::CacheProperties;
+//!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! let bus_name = ":1.1001";
+//! let object_path = "/org/a11y/atspi/accessible/root";
+//!
+//! let selection = SelectionProxy::builder(&conn)
 //!     .destination(bus_name)?
 //!     .path(object_path)?
+//!     .cache_properties(CacheProperties::No) // Caching uitgeschakeld!
 //!     .build()
 //!     .await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! [`n_selected_children`]: SelectionProxy#method.n_selected_children
