9495a7a1eb3c8482dee388f4560537a75e727890
Luuk van der Duim <luukvanderduim@gmail.com>
2026-06-07 18:56:14 +0200

    fix(proxies): `Component) mod docs compiling fragments.

--- atspi-proxies/src/component.rs
+++ atspi-proxies/src/component.rs
@@ -11,9 +11,9 @@
 //!
 //! ## Defaults
 //!
-//! The `Component` interface is implemented on individual, variable nodes within the
-//! application's UI-tree. As a consequence, the object path varies per node and
-//! no default path is applicable for this proxy.
+//! The `Component` interface can be implemented on any individual node within the
+//! application's UI-tree. As a consequence, both object path and service name vary
+//! and no default path or service is applicable for this proxy.
 //!
 //! ## How to obtain a `ComponentProxy`
 //!
@@ -23,11 +23,26 @@
 //! If you already have an [`AccessibleProxy`][ap] for a visual node, you can safely
 //! query and convert it using the [`ProxyExt`][pe] trait:
 //!
-//! ```rust,ignore
-//! use atspi::ProxyExt;
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::proxy_ext::ProxyExt;
+//! use atspi_proxies::accessible::ObjectRefExt;
+//! use atspi_common::ObjectRefOwned;
 //!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! // Establish an `AccessibleProxy` for the node
+//! let obj_ref = ObjectRefOwned::from_static_str_unchecked(":1.1000", "/org/a11y/atspi/accessible/root");
+//! let accessible_node = obj_ref.into_accessible_proxy(&conn).await?;
+//!
+//! // Get the associated interface proxies
 //! let proxies = accessible_node.proxies().await?;
 //! let component = proxies.component().await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! All proxies obtained through [`ProxyExt`][pe] share their underlying
@@ -37,12 +52,27 @@
 //! If you know the exact D-Bus service destination and object path, you can
 //! construct the proxy manually:
 //!
-//! ```rust,ignore
-//! let component = ComponentProxy::builder(&connection)
+//! ```rust,no_run
+//! # use futures_lite::future::block_on;
+//! use atspi_connection::AccessibilityConnection;
+//! use atspi_proxies::component::ComponentProxy;
+//! use zbus::proxy::CacheProperties;
+//!
+//! # block_on( async {
+//! let a11y = AccessibilityConnection::new().await?;
+//! let conn = a11y.connection();
+//!
+//! let bus_name = ":1.1001";
+//! let object_path = "/org/a11y/atspi/accessible/root";
+//!
+//! let component = ComponentProxy::builder(&conn)
 //!     .destination(bus_name)?
 //!     .path(object_path)?
+//!     .cache_properties(CacheProperties::No)
 //!     .build()
 //!     .await?;
+//! # Ok::<(), atspi_common::AtspiError>(())
+//! # });
 //! ```
 //!
 //! [`get_extents`]: ComponentProxy#method.get_extents
